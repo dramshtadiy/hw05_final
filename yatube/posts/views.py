e61bf46d89a775b41b31a788674ddc96154a0803
Даниил Ряйккенен <dramshtadiy1337@gmail.com>
2023-02-24 15:23:59 +0300

    finaal2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,14 +1,12 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
-from django.core.paginator import Paginator
 
 from .forms import PostForm, CommentForm
 from .models import Group, Post, Comment, Follow
 from .utils import paginate
 
 User = get_user_model()
-COUNT_10 = 10
 
 
 def index(request):
@@ -112,9 +110,7 @@ def add_comment(request, post_id):
 @login_required
 def follow_index(request):
     posts = Post.objects.filter(author__following__user=request.user)
-    paginator = Paginator(posts, COUNT_10)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    page_obj = paginate(request, posts)
     context = {
         'page_obj': page_obj,
         'title': 'Избранные посты',
